98bb5172fd90b4f400d2af7c43dad9531c68b6cb
Daria Smirnova <darya.smirn1@yandex.ru>
2022-12-23 21:37:39 +0300

    add fetching one entry from data base

--- app/database/connect.php
+++ app/database/connect.php
@@ -6,7 +6,7 @@ $db_name = 'daria-blog';
 $db_user = 'root';
 $db_pass = '';
 $charset = 'utf8';
-$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
 try {
 	$pdo = new PDO("$driver:host=$host;dbname=$db_name;charset=$charset", $db_user, $db_pass, $options);
--- app/database/db.php
+++ app/database/db.php
@@ -9,20 +9,75 @@ function prePrint($value)
 	echo '</pre>';
 }
 
-function selectAll($table)
+function dbCheckError($query)
 {
-	global $pdo;
-	$sql = "SELECT * FROM $table";
-	$query = $pdo->prepare($sql);
-	$query->execute();
 	$errInfo = $query->errorInfo();
 
 	if ($errInfo[0] !== PDO::ERR_NONE) {
 		echo $errInfo[2];
 		exit();
 	}
-
-	return $query->fetchAll();
+	return true;
 }
 
-prePrint(selectAll('users'));
+function selectAll($table, $params = [])
+{
+	global $pdo;
+	$sql = "SELECT * FROM $table";
+
+	if (!empty($params)) {
+		$i = 0;
+		foreach ($params as $key => $value) {
+			if (!is_numeric($value)) {
+				$value = "'" . $value . "'";
+			}
+			if ($i === 0) {
+				$sql .= " WHERE $key=$value";
+			} else {
+				$sql .= " AND $key=$value";
+			}
+
+			$i++;
+		}
+	}
+	$query = $pdo->prepare($sql);
+	$query->execute();
+
+	dbCheckError($query);
+	return $query->fetchAll();
+}
+function selectOne($table, $params = [])
+{
+	global $pdo;
+	$sql = "SELECT * FROM $table";
+
+	if (!empty($params)) {
+		$i = 0;
+		foreach ($params as $key => $value) {
+			if (!is_numeric($value)) {
+				$value = "'" . $value . "'";
+			}
+			if ($i === 0) {
+				$sql .= " WHERE $key=$value";
+			} else {
+				$sql .= " AND $key=$value";
+			}
+
+			$i++;
+		}
+	}
+
+	// $sql .= " LIMIT 1";
+	$query = $pdo->prepare($sql);
+	$query->execute();
+
+	dbCheckError($query);
+	return $query->fetch();
+}
+
+$params = [
+	'admin' => 0,
+	'username' => 'user Ben'
+];
+// prePrint(selectAll('users', $params));
+prePrint(selectOne('users'));
