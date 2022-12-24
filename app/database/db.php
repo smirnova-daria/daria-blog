a7f3b179c7f7ae4431b623cc1b39013a16646bd2
Daria Smirnova <darya.smirn1@yandex.ru>
2022-12-24 17:40:00 +0300

    add the function of inserting data into the table

--- app/database/db.php
+++ app/database/db.php
@@ -75,9 +75,43 @@ function selectOne($table, $params = [])
 	return $query->fetch();
 }
 
-$params = [
-	'admin' => 0,
-	'username' => 'user Ben'
+// $params = [
+// 	'admin' => 0,
+// 	'username' => 'user Ben'
+// ];
+// // prePrint(selectAll('users', $params));
+// prePrint(selectOne('users'));
+
+function insert($table, $params)
+{
+	global $pdo;
+
+	$i = 0;
+	$col = '';
+	$mask = '';
+	foreach ($params as $key => $value) {
+		if ($i === 0) {
+			$col .= "$key";
+			$mask .= "'" . "$value" . "'";
+		} else {
+			$col .= ", $key";
+			$mask .= ", '" . "$value" . "'";
+		}
+		$i++;
+	}
+
+	$sql = "INSERT INTO $table ($col) VALUES ($mask)";
+
+	$query = $pdo->prepare($sql);
+	$query->execute();
+
+	dbCheckError($query);
+}
+$arrData = [
+	'admin' => '0',
+	'username' => 'Tom',
+	'email' => 'tom@test.com',
+	'password' => '12345'
 ];
-// prePrint(selectAll('users', $params));
-prePrint(selectOne('users'));
+
+insert('users', $arrData);
