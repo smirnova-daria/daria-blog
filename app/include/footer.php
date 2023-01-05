36d383102beb0ea0fe84347f3d312746070f0a23
Daria Smirnova <darya.smirn1@yandex.ru>
2023-01-05 20:19:14 +0300

    add styles for footer

--- app/include/footer.php
+++ app/include/footer.php
@@ -1,15 +1,17 @@
 <footer class="footer">
 	<div class="container-md">
-		<nav class="footer__menu menu">
-			<ul class="menu__list">
-				<li><a href="/" class="menu__link">Главная</a></li>
-				<li><a href="/articles" class="menu__link">Статьи</a></li>
-				<li><a href="/contacts" class="menu__link">Контакты</a></li>
-			</ul>
-		</nav>
-		<div class="footer__info">
-			<p class="footer__author">Смирнова Дарья &mdash; frontend разработчик</p>
-			<time class="footer__year">2022</time>
+		<div class="footer-wrapper">
+			<nav class="footer__menu menu">
+				<ul class="menu__list">
+					<li><a href="/" class="menu__link">Главная</a></li>
+					<li><a href="/articles" class="menu__link">Статьи</a></li>
+					<li><a href="/contacts" class="menu__link">Контакты</a></li>
+				</ul>
+			</nav>
+			<div class="footer__info">
+				<p class="footer__author">Смирнова Дарья &mdash; frontend разработчик</p>
+				<time class="footer__year">2022</time>
+			</div>
 		</div>
 	</div>
 </footer>
--- app/include/menu.php
+++ app/include/menu.php
@@ -3,7 +3,7 @@
 		<li><a href="<?php echo BASE_URL ?>" class="menu__link">Главная</a></li>
 		<li><a href="<?php echo BASE_URL . 'article.php' ?>" class="menu__link">Статьи</a></li>
 		<li><a href="" class="menu__link">Контакты</a></li>
-		<li>
+		<li class="li-with-sub">
 			<?php if (isset($_SESSION['id'])): ?>
 				<a href="" class="menu__link link-sub"><?= $_SESSION['login']; ?></a>
 				<ul class="menu__sub-menu">
