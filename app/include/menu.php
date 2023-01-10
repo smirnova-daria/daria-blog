1da73360a1ce463e22f15083616d2adb8834335a
Daria Smirnova <darya.smirn1@yandex.ru>
2023-01-10 20:24:50 +0300

    fix bug with menu display

--- app/include/menu.php
+++ app/include/menu.php
@@ -3,8 +3,8 @@
 		<li><a href="<?php echo BASE_URL ?>" class="menu__link">Главная</a></li>
 		<li><a href="<?php echo BASE_URL . 'article.php' ?>" class="menu__link">Статьи</a></li>
 		<li><a href="" class="menu__link">Контакты</a></li>
-		<li class="li-with-sub">
-			<?php if (isset($_SESSION['id'])): ?>
+        <?php if (isset($_SESSION['id'])): ?>
+		    <li class="li-with-sub">
 				<a href="" class="menu__link link-sub"><?= $_SESSION['login']; ?></a>
 				<ul class="menu__sub-menu">
 					<?php if ($_SESSION['admin']): ?>
@@ -15,11 +15,9 @@
 					<li><a href="<?= BASE_URL . 'logout.php' ?>" class="menu__link">Выйти</a></li>
 				</ul>
 			<?php else: ?>
-				<a href="<?= BASE_URL . 'auth.php' ?>" class="menu__link">Войти</a>
-				<ul>
-					<li><a href="<?= BASE_URL . 'registration.php' ?>">Регистрация</a></li>
-				</ul>
-			<?php endif; ?>
-		</li>
+				<li><a href="<?= BASE_URL . 'auth.php' ?>" class="menu__link">Войти</a></li>
+				<li><a href="<?= BASE_URL . 'registration.php' ?>" class="menu__link">Регистрация</a></li>
+		    </li>
+        <?php endif; ?>
 	</ul>
 </nav>
